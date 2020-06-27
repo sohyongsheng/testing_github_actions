99a167d5078a5eb087f56c817717d4f15c25d76b
Soh Yong Sheng <sohyongsheng@gmail.com>
2020-06-27 22:24:34 +0800

    Print Python version from script

--- tryout.py
+++ tryout.py
@@ -1,6 +1,4 @@
+import sys
+
 if __name__ == '__main__':
-    # Python f-strings are not supported in Python 3.5, 
-    # so this will fail for Python 3.5.
-    print(f"Hello world! This is printed from a Python script")
-    print(f"Asserting True.")
-    assert True
+    print(f"Python version:\n{sys.version}")
