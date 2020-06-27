d2203cab7213921e9751221bebcd192407b3b407
Soh Yong Sheng <sohyongsheng@gmail.com>
2020-06-27 18:50:56 +0800

    Assert True back again

--- say_hello_world.py
+++ say_hello_world.py
@@ -2,5 +2,3 @@ if __name__ == '__main__':
     print("Hello world! This is printed from a Python script")
     print("Asserting True.")
     assert True
-    print("Asserting False.")
-    assert False
