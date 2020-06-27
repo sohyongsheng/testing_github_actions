755a7f43dbb58d9991d0dd7ee2aca7707c39e7ab
Soh Yong Sheng <sohyongsheng@gmail.com>
2020-06-27 21:38:41 +0800

    Add f-strings

--- tryout.py
+++ tryout.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
-    print("Hello world! This is printed from a Python script")
-    print("Asserting True.")
+    # Python f-strings are not supported in Python 3.5, 
+    # so this will fail for Python 3.5.
+    print(f"Hello world! This is printed from a Python script")
+    print(f"Asserting True.")
     assert True
